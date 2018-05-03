cfbdd88602e7e22f3cb3937f28f7b410ec47f266
suyash0103 <suyashghuge@gmail.com>
2018-05-04 04:41:31 +0530

    code

---
+++ Practice Comp code/largestNumber.cpp
@@ -0,0 +1,19 @@
+static bool compareNum(string a, string b) {
+            return a + b > b + a;
+        }
+
+string Solution::largestNumber(const vector<int> &num) {
+    string result;
+            vector<string> str;
+            for (int i = 0; i < num.size(); i++) {
+                str.push_back(to_string(num[i]));
+            }
+            sort(str.begin(), str.end(), compareNum);
+            for (int i = 0; i < str.size(); i++) {
+                result += str[i];
+            }
+
+            int pos = 0;
+            while (result[pos] == '0' && pos + 1 < result.size()) pos++; 
+            return result.substr(pos);
+        }
